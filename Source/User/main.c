e89f0e777e429eb7ab8cfe54943ee9b30a886529
sausiliu <sausiliu@163.com>
2015-12-28 18:34:02 +0800

    modify project

--- Source/User/main.c
+++ Source/User/main.c
@@ -2,14 +2,6 @@
     FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
     All rights reserved
 
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
     ***************************************************************************
     >>!   NOTE: The modification to the GPL is included to allow you to     !<<
     >>!   distribute a combined work that includes FreeRTOS without being   !<<
@@ -35,36 +27,6 @@
      *    http://www.FreeRTOS.org/Documentation                              *
      *                                                                       *
     ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
 */
 
 /*
@@ -82,11 +44,6 @@
  * displayed on the LCD by the 'Check' task as described below.  The
  * fast interrupt is configured and handled in the timertest.c source file.
  *
- * "LCD" task - the LCD task is a 'gatekeeper' task.  It is the only task that
- * is permitted to access the display directly.  Other tasks wishing to write a
- * message to the LCD send the message on a queue to the LCD task instead of
- * accessing the LCD themselves.  The LCD task just blocks on the queue waiting
- * for messages - waking and displaying the messages as they arrive.
  *
  * "Check" task -  This only executes every five seconds but has the highest
  * priority so is guaranteed to get processor time.  Its main function is to
@@ -112,7 +69,7 @@
 
 /* Demo app includes. */
 #include "lcd.h"
-#include "LCD_Message.h"
+//#include "LCD_Message.h"
 #include "BlockQ.h"
 #include "death.h"
 #include "integer.h"
@@ -122,6 +79,7 @@
 #include "PollQ.h"
 #include "flash.h"
 #include "comtest2.h"
+#include "stm32f10x_usart.h"
 
 /* Task priorities. */
 #define mainQUEUE_POLL_PRIORITY				( tskIDLE_PRIORITY + 2 )
@@ -171,19 +129,6 @@ information. */
 static void prvSetupHardware( void );
 
 /*
- * Configure the LCD as required by the demo.
- */
-static void prvConfigureLCD( void );
-
-/*
- * The LCD is written two by more than one task so is controlled by a
- * 'gatekeeper' task.  This is the only task that is actually permitted to
- * access the LCD directly.  Other tasks wanting to display a message send
- * the message to the gatekeeper.
- */
-static void vLCDTask( void *pvParameters );
-
-/*
  * Retargets the C library printf function to the USART.
  */
 int fputc( int ch, FILE *f );
@@ -199,6 +144,8 @@ int fputc( int ch, FILE *f );
  * via a queue.
  */
 static void vCheckTask( void *pvParameters );
+static void vLEDTask( void *pvParameters );
+void uart1_init(void);
 
 /*
  * Configures the timers and interrupts for the fast interrupt test as
@@ -216,261 +163,206 @@ QueueHandle_t xLCDQueue;
 int main( void )
 {
 #ifdef DEBUG
-  debug();
+    debug();
 #endif
 
-	prvSetupHardware();
-
-	/* Create the queue used by the LCD task.  Messages for display on the LCD
-	are received via this queue. */
-	xLCDQueue = xQueueCreate( mainLCD_QUEUE_SIZE, sizeof( xLCDMessage ) );
-
-	/* Start the standard demo tasks. */
-	vStartBlockingQueueTasks( mainBLOCK_Q_PRIORITY );
-    vCreateBlockTimeTasks();
-    vStartSemaphoreTasks( mainSEM_TEST_PRIORITY );
-    vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
-    vStartIntegerMathTasks( mainINTEGER_TASK_PRIORITY );
-	vStartLEDFlashTasks( mainFLASH_TASK_PRIORITY );
-	vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
-
-	/* Start the tasks defined within this file/specific to this demo. */
+    prvSetupHardware();
+    vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
+	
+    xTaskCreate( vLEDTask, "led_test", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
     xTaskCreate( vCheckTask, "Check", mainCHECK_TASK_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
-	xTaskCreate( vLCDTask, "LCD", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
 
-	/* The suicide tasks must be created last as they need to know how many
-	tasks were running prior to their creation in order to ascertain whether
-	or not the correct/expected number of tasks are running at any given time. */
+    /* The suicide tasks must be created last as they need to know how many
+    tasks were running prior to their creation in order to ascertain whether
+    or not the correct/expected number of tasks are running at any given time. */
     vCreateSuicidalTasks( mainCREATOR_TASK_PRIORITY );
 
-	/* Configure the timers used by the fast interrupt timer test. */
-	vSetupTimerTest();
+    /* Configure the timers used by the fast interrupt timer test. */
+    vSetupTimerTest();
 
-	/* Start the scheduler. */
-	vTaskStartScheduler();
+    /* Start the scheduler. */
+    vTaskStartScheduler();
 
-	/* Will only get here if there was not enough heap space to create the
-	idle task. */
-	return 0;
+    /* Will only get here if there was not enough heap space to create the
+    idle task. */
+    return 0;
 }
-/*-----------------------------------------------------------*/
-
-void vLCDTask( void *pvParameters )
+/*-----------------------test-------------------------------*/
+void vLEDTask(void * pvParameters)
 {
-xLCDMessage xMessage;
+    volatile unsigned long ul;
 
-	/* Initialise the LCD and display a startup message. */
-	prvConfigureLCD();
-	LCD_DrawMonoPict( ( unsigned long * ) pcBitmap );
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
 
-	for( ;; )
-	{
-		/* Wait for a message to arrive that requires displaying. */
-		while( xQueueReceive( xLCDQueue, &xMessage, portMAX_DELAY ) != pdPASS );
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-		/* Display the message.  Print each message to a different position. */
-		printf( ( char const * ) xMessage.pcMessage );
-	}
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    USART_InitStructure.USART_BaudRate = 115200; /* 9600 Baud */
+    USART_InitStructure.USART_WordLength = USART_WordLength_9b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No ;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_InitStructure.USART_Clock = USART_Clock_Disable;
+    USART_InitStructure.USART_CPOL = USART_CPOL_Low;
+    USART_InitStructure.USART_CPHA = USART_CPHA_1Edge;
+    USART_InitStructure.USART_LastBit = USART_LastBit_Disable;
+
+
+    USART_Init(USART2, &USART_InitStructure);
+
+    for(;;)
+    {
+        GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_SET);
+        vTaskDelay(100);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_RESET);
+        vTaskDelay(100);
+    }
 }
+
+
 /*-----------------------------------------------------------*/
 
 static void vCheckTask( void *pvParameters )
 {
-TickType_t xLastExecutionTime;
-xLCDMessage xMessage;
-static signed char cPassMessage[ mainMAX_MSG_LEN ];
-extern unsigned short usMaxJitter;
+    TickType_t xLastExecutionTime;
+//    xLCDMessage xMessage;
+    static signed char cPassMessage[ mainMAX_MSG_LEN ];
+    extern unsigned short usMaxJitter;
 
-	xLastExecutionTime = xTaskGetTickCount();
-	xMessage.pcMessage = cPassMessage;
+    xLastExecutionTime = xTaskGetTickCount();
+//   xMessage.pcMessage = cPassMessage;
 
     for( ;; )
-	{
-		/* Perform this check every mainCHECK_DELAY milliseconds. */
-		vTaskDelayUntil( &xLastExecutionTime, mainCHECK_DELAY );
+    {
+        /* Perform this check every mainCHECK_DELAY milliseconds. */
+        vTaskDelayUntil( &xLastExecutionTime, mainCHECK_DELAY );
 
-		/* Has an error been found in any task? */
+        /* Has an error been found in any task? */
 
         if( xAreBlockingQueuesStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR IN BLOCK Q\n";
-		}
-		else if( xAreBlockTimeTestTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR IN BLOCK TIME\n";
-		}
+        {
+//           xMessage.pcMessage = "ERROR IN BLOCK Q\n";
+        }
+        else if( xAreBlockTimeTestTasksStillRunning() != pdTRUE )
+        {
+//           xMessage.pcMessage = "ERROR IN BLOCK TIME\n";
+        }
         else if( xAreSemaphoreTasksStillRunning() != pdTRUE )
         {
-            xMessage.pcMessage = "ERROR IN SEMAPHORE\n";
+//           xMessage.pcMessage = "ERROR IN SEMAPHORE\n";
         }
         else if( xArePollingQueuesStillRunning() != pdTRUE )
         {
-            xMessage.pcMessage = "ERROR IN POLL Q\n";
+//           xMessage.pcMessage = "ERROR IN POLL Q\n";
         }
         else if( xIsCreateTaskStillRunning() != pdTRUE )
         {
-            xMessage.pcMessage = "ERROR IN CREATE\n";
+            //          xMessage.pcMessage = "ERROR IN CREATE\n";
         }
         else if( xAreIntegerMathsTaskStillRunning() != pdTRUE )
         {
-            xMessage.pcMessage = "ERROR IN MATH\n";
+//           xMessage.pcMessage = "ERROR IN MATH\n";
         }
-		else if( xAreComTestTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR IN COM TEST\n";
-		}
-		else
-		{
-			sprintf( ( char * ) cPassMessage, "PASS [%uns]\n", ( ( unsigned long ) usMaxJitter ) * mainNS_PER_CLOCK );
-		}
+        else if( xAreComTestTasksStillRunning() != pdTRUE )
+        {
+//           xMessage.pcMessage = "ERROR IN COM TEST\n";
+        }
+        else
+        {
+            sprintf( ( char * ) cPassMessage, "PASS [%uns]\n", ( ( unsigned long ) usMaxJitter ) * mainNS_PER_CLOCK );
+        }
 
-		/* Send the message to the LCD gatekeeper for display. */
-		xQueueSend( xLCDQueue, &xMessage, portMAX_DELAY );
-	}
+        /* Send the message to the LCD gatekeeper for display. */
+//       xQueueSend( xLCDQueue, &xMessage, portMAX_DELAY );
+    }
 }
 /*-----------------------------------------------------------*/
+void uart1_init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    USART_Init(USART1, &USART_InitStructure);
+}
+
 
 static void prvSetupHardware( void )
 {
-	/* Start with the clocks in their expected state. */
-	RCC_DeInit();
+    /* Start with the clocks in their expected state. */
+    RCC_DeInit();
 
-	/* Enable HSE (high speed external clock). */
-	RCC_HSEConfig( RCC_HSE_ON );
+    /* Enable HSE (high speed external clock). */
+    RCC_HSEConfig( RCC_HSE_ON );
 
-	/* Wait till HSE is ready. */
-	while( RCC_GetFlagStatus( RCC_FLAG_HSERDY ) == RESET )
-	{
-	}
+    /* Wait till HSE is ready. */
+    while( RCC_GetFlagStatus( RCC_FLAG_HSERDY ) == RESET )
+    {
+    }
 
-	/* 2 wait states required on the flash. */
-	*( ( unsigned long * ) 0x40022000 ) = 0x02;
+    /* 2 wait states required on the flash. */
+    *( ( unsigned long * ) 0x40022000 ) = 0x02;
 
-	/* HCLK = SYSCLK */
-	RCC_HCLKConfig( RCC_SYSCLK_Div1 );
+    /* HCLK = SYSCLK */
+    RCC_HCLKConfig( RCC_SYSCLK_Div1 );
 
-	/* PCLK2 = HCLK */
-	RCC_PCLK2Config( RCC_HCLK_Div1 );
+    /* PCLK2 = HCLK */
+    RCC_PCLK2Config( RCC_HCLK_Div1 );
 
-	/* PCLK1 = HCLK/2 */
-	RCC_PCLK1Config( RCC_HCLK_Div2 );
+    /* PCLK1 = HCLK/2 */
+    RCC_PCLK1Config( RCC_HCLK_Div2 );
 
-	/* PLLCLK = 8MHz * 9 = 72 MHz. */
-	RCC_PLLConfig( RCC_PLLSource_HSE_Div1, RCC_PLLMul_9 );
+    /* PLLCLK = 8MHz * 9 = 72 MHz. */
+    RCC_PLLConfig( RCC_PLLSource_HSE_Div1, RCC_PLLMul_9 );
 
-	/* Enable PLL. */
-	RCC_PLLCmd( ENABLE );
+    /* Enable PLL. */
+    RCC_PLLCmd( ENABLE );
 
-	/* Wait till PLL is ready. */
-	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
-	{
-	}
+    /* Wait till PLL is ready. */
+    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+    {
+    }
 
-	/* Select PLL as system clock source. */
-	RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK );
+    /* Select PLL as system clock source. */
+    RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK );
 
-	/* Wait till PLL is used as system clock source. */
-	while( RCC_GetSYSCLKSource() != 0x08 )
-	{
-	}
+    /* Wait till PLL is used as system clock source. */
+    while( RCC_GetSYSCLKSource() != 0x08 )
+    {
+    }
 
-	/* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
-	RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
-							| RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO, ENABLE );
+    /* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
+    RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
+                            | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO, ENABLE );
 
-	/* SPI2 Periph clock enable */
-	RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
+    /* SPI2 Periph clock enable */
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
 
 
-	/* Set the Vector Table base address at 0x08000000 */
-	NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
+    /* Set the Vector Table base address at 0x08000000 */
+    NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
 
-	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+    NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
 
-	/* Configure HCLK clock as SysTick clock source. */
-	SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
+    /* Configure HCLK clock as SysTick clock source. */
+    SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
 
-	vParTestInitialise();
+    vParTestInitialise();
 }
+
 /*-----------------------------------------------------------*/
 
-static void prvConfigureLCD( void )
-{
-GPIO_InitTypeDef GPIO_InitStructure;
-
-	/* Configure LCD Back Light (PA8) as output push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init( GPIOA, &GPIO_InitStructure );
-
-	/* Set the Backlight Pin */
-	GPIO_WriteBit(GPIOA, GPIO_Pin_8, Bit_SET);
-
-	/* Initialize the LCD */
-	LCD_Init();
-
-	/* Set the Back Color */
-	LCD_SetBackColor( White );
-
-	/* Set the Text Color */
-	LCD_SetTextColor( 0x051F );
-
-	LCD_Clear();
-}
-/*-----------------------------------------------------------*/
-
-int fputc( int ch, FILE *f )
-{
-static unsigned short usColumn = 0, usRefColumn = mainCOLUMN_START;
-static unsigned char ucLine = 0;
-
-	if( ( usColumn == 0 ) && ( ucLine == 0 ) )
-	{
-		LCD_Clear();
-	}
-
-	if( ch != '\n' )
-	{
-		/* Display one character on LCD */
-		LCD_DisplayChar( ucLine, usRefColumn, (u8) ch );
-
-		/* Decrement the column position by 16 */
-		usRefColumn -= mainCOLUMN_INCREMENT;
-
-		/* Increment the character counter */
-		usColumn++;
-		if( usColumn == mainMAX_COLUMN )
-		{
-			ucLine += mainROW_INCREMENT;
-			usRefColumn = mainCOLUMN_START;
-			usColumn = 0;
-		}
-	}
-	else
-	{
-		/* Move back to the first column of the next line. */
-		ucLine += mainROW_INCREMENT;
-		usRefColumn = mainCOLUMN_START;
-		usColumn = 0;
-	}
-
-	/* Wrap back to the top of the display. */
-	if( ucLine >= mainMAX_LINE )
-	{
-		ucLine = 0;
-	}
-
-	return ch;
-}
 /*-----------------------------------------------------------*/
 
 #ifdef  DEBUG
 /* Keep the linker happy. */
 void assert_failed( unsigned char* pcFile, unsigned long ulLine )
 {
-	for( ;; )
-	{
-	}
+    for( ;; )
+    {
+    }
 }
 #endif
